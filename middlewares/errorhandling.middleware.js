847e61483b949ba03b92f8fda05e2eab8048bba5
Aditi Sujlegaonkar <115927458+AditiKulkarni1697@users.noreply.github.com>
2026-02-01 17:59:04 +0530

    Delete middlewares directory

--- middlewares/errorhandling.middleware.js
+++
@@ -1,8 +0,0 @@
-const {INTERNALSERVERERROR} = require("../helpers/sendResponse")
-
-function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    INTERNALSERVERERROR(res,err)
-  }
-
-  module.exports = {INTERNALSERVERERROR}
